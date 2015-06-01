73fe3f59c64c1bde47bf6141601302d4cdc3536a
Slava Bacherikov <slava@bacher09.org>
2015-06-01 15:27:08 +0300

    Add basic support for dictionary api

---
+++ src/Network/Yandex/Dictionary.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Network.Yandex.Dictionary (
+    getLangs
+) where
+import Control.Lens
+import Network.Wreq hiding (options)
+import qualified Network.Wreq.Session as S
+import Network.Yandex.Translate.Types
+import Control.Monad.IO.Class
+import Control.Monad.Catch (MonadThrow(throwM))
+
+
+baseUrl, getLangsUrl, lookupUrl :: String
+baseUrl = "https://dictionary.yandex.net/api/v1/dicservice.json/"
+getLangsUrl = baseUrl ++ "getLangs"
+lookupUrl = baseUrl ++ "lookup"
+
+
+baseOptions :: (MonadIO m, MonadThrow m) => YandexApiT m Options
+baseOptions = do
+    ykey <- view $ _config.apikey
+    opt <- view $ _config.httpOptions
+    return $ opt & param "key" .~ [ykey]
+
+
+getLangs :: (MonadIO m, MonadThrow m) => YandexApiT m [Direction]
+getLangs = do
+    opts <- baseOptions
+    sess <- view _session
+    r <- liftIO $ asJSON =<< S.getWith opts sess getLangsUrl
+    return $ r ^. responseBody
---
+++ tests/Network/Yandex/DictionarySpec.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Network.Yandex.DictionarySpec (
+    spec
+) where
+import Test.Hspec
+import System.Environment
+import Network.Yandex.Translate
+import Network.Yandex.Dictionary
+import Data.Text
+
+
+spec :: Spec
+spec = do
+    -- raise exception if var not set
+    key <- runIO $ pack `fmap` getEnv "YANDEX_APIKEY_DICT"
+    let conf = configureApi key
+
+    describe "getLangs" $ do
+        it "should contains en-ru and ru-en" $ do
+            dirs <- runApi conf getLangs
+            dirs `shouldContain` [Direction ("en", "ru")]
+            dirs `shouldContain` [Direction ("ru", "en")]
+  where
+    runApi = runYandexApiSession
